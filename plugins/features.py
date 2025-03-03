83e0afef5104fa6d0057d4ab2fed91f983cda862
rohit3607 <rohitplayer87089@gmail.com>
2025-03-03 22:47:03 +0530

    Update features.py

--- plugins/features.py
+++ plugins/features.py
@@ -52,9 +52,11 @@ async def weekly_anime_command(client: Client, message: Message):
 
         keyboard = [[InlineKeyboardButton(f"{style_anime_title(anime.get('title'))}", callback_data=f'detail_{anime.get("mal_id")}')] 
                     for anime in weekly_anime_list[:10]]
-        keyboard.extend([InlineKeyboardButton("• ғᴏʀ ", url ='t.me/Pythonbotz'),
-                          InlineKeyboardButton(" ᴍᴏʀᴇ •", url ='t.me/pythonbotz')],
-            [InlineKeyboardButton("🦄 ᴄʟᴏsᴇ !!", callback_data='close')])
+        keyboard.extend([
+            [InlineKeyboardButton("• ғᴏʀ ", url='t.me/Pythonbotz'),
+             InlineKeyboardButton(" ᴍᴏʀᴇ •", url='t.me/pythonbotz')],
+            [InlineKeyboardButton("🦄 ᴄʟᴏsᴇ !!", callback_data='close')]
+             ])
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         await message.reply_text(
@@ -81,9 +83,11 @@ async def search_anime_command(client: Client, message: Message):
 
         keyboard = [[InlineKeyboardButton(f"{get_anime_emoji(anime.get('title'))} {anime.get('title')}", callback_data=f'detail_{anime.get("mal_id")}')] 
                     for anime in search_results[:10]]
-        keyboard.extend([InlineKeyboardButton("• ғᴏʀ ", url ='t.me/Pythonbotz'),
-                          InlineKeyboardButton(" ᴍᴏʀᴇ •", url ='t.me/pythonbotz')],
-            [InlineKeyboardButton("🦄 ᴄʟᴏsᴇ !!", callback_data='close')])
+        keyboard.extend([
+            [InlineKeyboardButton("• ғᴏʀ ", url='t.me/Pythonbotz'),
+             InlineKeyboardButton(" ᴍᴏʀᴇ •", url='t.me/pythonbotz')],
+            [InlineKeyboardButton("🦄 ᴄʟᴏsᴇ !!", callback_data='close')]
+             ])
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         await message.reply_text(
